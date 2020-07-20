95b05a9fefbc4bb206fa5bdcb23c3bbc147f52a8
Rishu kumar <rishukr06@gmail.com>
2020-07-20 14:04:34 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// dummy string 
+
+echo "Hello World!"
+
+?>
+<br/>
+
+// navigation
+
+<a href="./pages/page1.php"> Go To Page 1 </a>
+
+<br/>
+
+<a href="./pages/page2.php"> Go To Page 2 </a>
